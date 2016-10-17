1c9600a9e6dd39a10254d5c7133fc55634536d5d
shamu92 <altshteyn@gmail.com>
2016-10-17 15:00:44 -0700

    added filter and splice

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-
+    //shrinks the header 
     $('#dropdown').on('change', function() {
         $('header').addClass('headerShrink');
     });
-
+    //shows the loading image when selection is chosen from drop down.
     $('#dropdown').on('change', function() {
         $('.loading').show();
     })
-
 
 
     $("#dropdown").on('change', function(event) {
@@ -25,27 +24,30 @@ $(document).ready(function() {
 
         }).done(function(data) {
             // console.log(data);
-            $('.top-news').empty()
-            $.each(data.results, function(key, value) {
+            var article = "";
+            var dataResults = data.results.filter(function(value) {
+                return value.multimedia.length >= 5;
+            })
+            dataResults.splice(12);
 
-                if (value.multimedia.length > 0) {
+            $.each(dataResults, function(key, value) {
 
-                    var picture = value.multimedia[4].url,
-                        abstract = value.abstract,
-                        articleUrl = value.url,
 
-                        article = "<li>";
-                    article += "<a href=" + articleUrl + '>';
-                    article += "<div class='articleBackground' style='background-image:url(";
-                    article += picture;
-                    article += ")'><p class='abstract'>";
-                    article += abstract;
-                    article += "</p></div></a></li>";
+                var picture = value.multimedia[4].url,
+                    abstract = value.abstract,
+                    articleUrl = value.url;
 
-                    // console.log(picture);
+                article += "<li>";
+                article += "<a href=" + articleUrl + '>';
+                article += "<div class='articleBackground' style='background-image:url(";
+                article += picture;
+                article += ")'><p class='abstract'>";
+                article += abstract;
+                article += "</p></div></a></li>";
 
-                    $(".top-news").append(article)
-                }
+                // console.log(picture);
+
+                $(".top-news").html(article)
             });
 
         }).fail(function(err) {
