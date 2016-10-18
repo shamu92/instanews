c401bed492441dd8553f7985d2fc58b11abc1b1c
Ringo <ringong95@gmail.com>
2016-10-18 14:03:11 -0700

    Why is eventpreventDefault needed. It isint stopping anything here

--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ $(document).ready(function() {
 
 
     $("#dropdown").on('change', function(event) {
-        
-        
+        // event.preventDefault();
         // console.log('hope');
         var selected = $('#dropdown').val();
         var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
